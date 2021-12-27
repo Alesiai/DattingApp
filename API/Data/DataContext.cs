b7507c203639f13078e2199b32c79e518a042f86
Alesisa Ivanova <alesya.ivanova.02@gmail.com>
2021-12-27 19:04:40 +0300

    End of section 15

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,42 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-
 using API.Entities;
-
-
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class DataContext : DbContext
     {
-      public DataContext(DbContextOptions options) : base(options){
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
 
-      }
-
-      public DbSet<AppUser> Users { get; set; }
-      public DbSet<UserLike> Likes { get; set; }
+        public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
             builder.Entity<UserLike>()
-              .HasKey(k => new {k.SourceUserId, k.LikedUserId});
+                .HasKey(k => new {k.SourceUserId, k.LikedUserId});
 
             builder.Entity<UserLike>()
                 .HasOne(s => s.SourceUser)
                 .WithMany(l => l.LikedUsers)
                 .HasForeignKey(s => s.SourceUserId)
                 .OnDelete(DeleteBehavior.Cascade);
-
+            
             builder.Entity<UserLike>()
                 .HasOne(s => s.LikedUser)
                 .WithMany(l => l.LikedByUsers)
                 .HasForeignKey(s => s.LikedUserId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            builder.Entity<Message>()
+                .HasOne(u => u.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Message>()
+                .HasOne(u => u.Sender)
+                .WithMany(m => m.MessagesSent)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
